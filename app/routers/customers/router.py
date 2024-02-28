62a87d1ecc5bfc2b3f05ca585bc21d2d06833c29
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-28 17:50:27 -0600

    Refactor customer router and CRUD operations
    This commit simplifies the database connection management by extracting it to a new Request object in the customer router. It also refactors the CRUD operations in the corresponding CRUD file, renaming methods to more intuitive names like `get_one` and `get_all`, as well as updating the read routes in the router. The refactored code simplifies the overall logic, streamlines the code, and improves maintainability.

--- app/routers/customers/router.py
+++ app/routers/customers/router.py
@@ -1,38 +1,29 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 
 from app import schemas
-from app.database import SessionLocal
-from app.routers.customers.crud import get_customers
+from app.routers.customers.crud import get_one, get_all
 
 router = APIRouter()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
+# Dependency
+def get_db(request: Request):
+    return request.state.db
+
 
 @router.get("/", response_model=List[schemas.CustomerSchema])
-async def read_customers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    customers = get_customers(db, skip=skip, limit=limit)
+async def read_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    customers = get_one(db, skip=skip, limit=limit)
     if customers is None:
         raise HTTPException(status_code=404, detail="Customers not found")
     return customers
 
-# @router.get("/customer_demographics", response_model=List[CustomerDemographics])
-# async def read_customer_demographics(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     customer_demographics = get_customer_demographics(db, skip=skip, limit=limit)
-#     if customer_demographics is None:
-#         raise HTTPException(status_code=404, detail="Customer demographics not found")
-#     return customer_demographics
-#
-# @router.get("/customer_customer_demo", response_model=List[CustomerCustomerDemo])
-# async def read_customer_customer_demo(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     customer_customer_demo = get_customer_customer_demo(db, skip=skip, limit=limit)
-#     if customer_customer_demo is None:
-#         raise HTTPException(status_code=404, detail="Customer customer demo not found")
-#     return customer_customer_demo
+@router.get("/{pk}", response_model=schemas.CustomerSchema)
+async def read_one(pk: str, db: Session = Depends(get_db)):
+    customers = get_all(db, pk)
+    if customers is None:
+        raise HTTPException(status_code=404, detail="Customers not found")
+    return customers
