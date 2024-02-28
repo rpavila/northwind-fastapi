960c99e6b412117cea822b72d140c9c112b805c5
Ricardo Pablo Avila Alfaro <ravila@grupodeca.com.mx>
2024-02-28 17:07:54 -0600

    Refactor customer router and database access in main app
    This commit refactors the customer routing and database access across main application file. It separates concerns by creating a new 'router.py' file, which includes DB session management and customer retrieval through CRUD operation. Commented out codes on main.py suggests obsolete functions becoming irrelevant with the new design.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
 
-from app.routers import customers
+from app.routers.customers.router import router as router_customer
 
 app = FastAPI()
 
-app.include_router(customers.router, prefix="/customer", tags=["customer"])
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-#
-#
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+app.include_router(router_customer, prefix="/customers", tags=["customer"])
