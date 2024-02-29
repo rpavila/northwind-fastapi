8104ddac10306e371e7146096e3b620b292678c3
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-29 08:38:56 -0600

    Implement unique ID generation and URL prefix for FastAPI app
    This commit introduces a unique ID generation method and dynamically sets the openapi_url for the FastAPI application in main.py. It also adds a prefix setting from the configuration module to the API router, enhancing customizability and organization of the app routing system.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.routing import APIRoute
 
 from app.api.v1.api import api_router
+from app.core.config import settings
 from app.database import SessionLocal
 
-app = FastAPI()
+
+def custom_generate_unique_id(route: APIRoute):
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
 
 
 @app.middleware("http")
@@ -17,4 +28,4 @@ async def db_session_middleware(request: Request, call_next):
     return response
 
 
-app.include_router(api_router)
+app.include_router(api_router, prefix=settings.API_V1_STR)
