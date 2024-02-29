dfc78fdad719fad8726f13159f73bdbb923ea94f
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-28 23:19:53 -0600

    Add customers endpoint to API v1 router
    Shifted customers endpoint from main application to API v1 router to maintain a clean and modular code structure. The updated router is referenced from `app/api/v1/api.py`. This change also enhances the scalability of API versioning.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
+from app.api.v1.api import api_router
 from app.routers.customers.router import router as router_customer
 
 app = FastAPI()
 
-app.include_router(router_customer, prefix="/customers", tags=["customer"])
+app.include_router(api_router)
