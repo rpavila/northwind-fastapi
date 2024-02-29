0e64e9358c20df867392e89f9d8f0013920bd32b
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-29 09:13:52 -0600

    Create new API endpoints and refactor existing ones
    Added several new API endpoints (Orders, Employees Territories, Products, Regions, Territories, Employees, Suppliers, Shippers, Order Details, Categories) and refactored existing ones (Customers) to use a uniform structure. This ensures all API endpoints follow the same pattern for readability and maintainability.

--- app/api/v1/endpoints/products.py
+++ app/api/v1/endpoints/products.py
@@ -5,22 +5,22 @@ from sqlalchemy.orm import Session
 
 from app import schemas
 from app.api.v1.deps import SessionDep
-from app.crud import crud_customer as crud_module
+from app.crud import crud_product as crud_module
 
 router = APIRouter()
 
 
-@router.get("", response_model=List[schemas.CustomerSchema])
+@router.get("", response_model=List[schemas.ProductSchema])
 async def read_all(skip: int = 0, limit: int = 100, db: Session = SessionDep):
     items = crud_module.get_all(db, skip=skip, limit=limit)
     if items is None:
-        raise HTTPException(status_code=404, detail="Customers not found")
+        raise HTTPException(status_code=404, detail="Products not found")
     return items
 
 
-@router.get('/{pk}', response_model=schemas.CustomerSchema)
-async def read_one(pk: str, db: Session = SessionDep):
+@router.get('/{pk}', response_model=schemas.ProductSchema)
+async def read_one(pk: int, db: Session = SessionDep):
     instance = crud_module.get(db, id=pk)
     if instance is None:
-        raise HTTPException(status_code=404, detail="Customer not found")
+        raise HTTPException(status_code=404, detail="Product not found")
     return instance
