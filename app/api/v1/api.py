30ec82e2417c35c58da2dc9d0b2e3a6f296092bc
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-29 00:02:10 -0600

    Add 'us_states' endpoint to API
    The update includes adding a new endpoint 'us_states' to our API. A new accompanying file 'us_states.py' has also been added, which includes routing, reading, and error handling functions for this new endpoint. The 'api.py' file has been updated to recognize and route for this new endpoint as well.

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import customers
+from app.api.v1.endpoints import customers, us_states
 
 api_router = APIRouter()
 api_router.include_router(customers.router, prefix="/customers", tags=["customers"])
+api_router.include_router(us_states.router, prefix="/us-states", tags=["us_states"])
---
+++ app/api/v1/endpoints/us_states.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.orm import Session
+
+from app import schemas
+from app.api.v1.deps import SessionDep
+from app.crud import crud_us_state as crud_module
+
+router = APIRouter()
+
+
+@router.get("", response_model=List[schemas.UsStateSchema])
+async def read_all(skip: int = 0, limit: int = 100, db: Session = SessionDep):
+    items = crud_module.get_all(db, skip=skip, limit=limit)
+    if items is None:
+        raise HTTPException(status_code=404, detail="Customers not found")
+    return items
+
+
+@router.get('/{pk}', response_model=schemas.UsStateSchema)
+async def read_one(pk: int, db: Session = SessionDep):
+    instance = crud_module.get(db, id=pk)
+    if instance is None:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return instance
