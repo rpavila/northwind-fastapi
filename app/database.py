051292e756f852a1fab077e3be14bf19d30751f9
Ricardo P. Avila <rpaa01@gmail.com>
2024-03-01 16:54:36 -0600

    Enable SQL echo in SQLAlchemy engine configuration
    SQLAlchemy's engine creation now includes an echo parameter set to True, which allows for the logging of all database activity. This change can be helpful for debugging and understanding the flow of calls between the application and the database.

--- app/database.py
+++ app/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    str(settings.SQLALCHEMY_DATABASE_URI)
+    str(settings.SQLALCHEMY_DATABASE_URI),
+    echo=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
